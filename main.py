54d769f204fd2862463456725fc7f08800d01fd6
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-06-14 21:46:10 +0600

    finished creating the csv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import urllib.request, json
 from operator import itemgetter
+import sys
 
 def mysortfunc(key):
     if 'rank' in key:
@@ -12,5 +13,19 @@ with urllib.request.urlopen("https://www.forbes.com/ajax/list/data?year=2018&uri
     for d in data:
         if 'rank' in d:
             new_list.append(d)
-    new_list.sort(key=itemgetter('rank'))
-    print(new_list)
+            new_list.sort(key=itemgetter('rank'))
+
+    filename = "blist.csv"
+
+    with open(filename, "w") as f:
+        headers = "Rank, Name, Company, Worth\n"
+        f.write(headers)
+
+        for d in new_list:
+            rank = d['rank']
+            name = d['name']
+            company = d['company'] if 'company' in d else 'None'
+            worth = d['worth']
+            f.write("{}, {}, {}, {}\n".format(
+                rank, name, company, worth
+            ))
