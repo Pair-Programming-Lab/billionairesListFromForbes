30a6f8f6839dde8f32ba272ae717321d5801274a
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-06-14 21:16:54 +0600

    updated dict to mainipulate further

--- main.py
+++ main.py
@@ -1,37 +1,16 @@
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
+import urllib.request, json
+from operator import itemgetter
 
-my_url = 'https://www.forbes.com/billionaires/list/#version:static'
-uClient = uReq(my_url)
-page_html = uClient.read()
-uClient.close()
-page_soup = soup(page_html, "html.parser")
-rows = page_soup.findAll(
-    'tr',
-    {
-        'class': 'ad'
-    }
-)
+def mysortfunc(key):
+    if 'rank' in key:
+        return key['rank']
+    return 0
 
-filename = "bList.csv"
-f = open(filename, 'w')
-headers = "Rank , Name, Company, Worth\n"
-f.write(headers)
-
-print(len(rows))
-for row in rows:
-
-    brand = container.div.div.a.img['title']
-    title_container = container.findAll('a', {'class': 'item-title'})
-    product_name = title_container[0].text
-
-    shipping_container = container.findAll('li', {'class': 'price-ship'})
-    shipping = shipping_container[0].text.strip()
-
-    Rank = row.
-Name
-Company
-Worth
-    f.write(Rank + ',' + Name + ',' + Company + ',' + Worth + '\n')
-
-f.close()
+with urllib.request.urlopen("https://www.forbes.com/ajax/list/data?year=2018&uri=billionaires&type=person") as url:
+    data = json.loads(url.read().decode())
+    new_list = []
+    for d in data:
+        if 'rank' in d:
+            new_list.append(d)
+    new_list.sort(key=itemgetter('rank'))
+    print(new_list)
